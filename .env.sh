28a0e4dd9ee6015ea13dba7eea03566925e7b4d7
KPO17 <olivierkpo165@gmail.com>
2025-09-20 02:21:11 +0100

    premier commit: initial3

---
+++ .env.sh
@@ -0,0 +1,11 @@
+# Configuration Flask
+FLASK_ENV=development
+PORT=5000
+
+# YouTube API Key (optionnel, pour métadonnées complètes)
+YOUTUBE_API_KEY=votre_cle_api_youtube_ici
+
+# Configuration Firebase (optionnel, pour l'authentification)
+FIREBASE_PROJECT_ID=votre_project_id
+FIREBASE_PRIVATE_KEY=votre_private_key
+FIREBASE_CLIENT_EMAIL=votre_client_email
